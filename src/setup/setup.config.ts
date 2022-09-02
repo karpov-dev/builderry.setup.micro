a71e8ea77787d311be2c1ef4e9b5903bb5d7ffc1
Vladimir Karpov <54440209+karpov-dev@users.noreply.github.com>
2022-09-02 09:39:54 +0300

    0.0.1-1 - Port fix

--- src/setup/setup.config.ts
+++ src/setup/setup.config.ts
@@ -3,8 +3,8 @@ import {ISetupConfig} from "../interfaces";
 export const configs = {
   workspace: {
     setupItems: [
-      {order: 0, url: 'http://localhost:8001'},
-      {order: 1, url: 'http://localhost:8002'},
+      {order: 0, url: 'http://localhost:3101'},
+      {order: 1, url: 'http://localhost:3102'},
     ]
   } as ISetupConfig
 }
